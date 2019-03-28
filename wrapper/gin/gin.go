0b2417f609988ff2e5e7d8a4a90affd9df0866d6
Tyler King <tyler@ohmybrew.com>
2019-03-28 01:34:59 -0230

    Added Gin support

---
+++ wrapper/gin/gin.go
@@ -0,0 +1,19 @@
+package gin
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ohmybrew/http_shopify_webhook"
+)
+
+// Compatible wrapper for Gin framework.
+// Example: `g.Use(WebhookVerify("secret")`.
+func WebhookVerify(key string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ok := http_shopify_webhook.WebhookVerifyRequest(key, ctx.Writer, ctx.Request)
+		if !ok {
+			ctx.AbortWithStatus(http.StatusBadRequest)
+		}
+	}
+}
